9a179b90725c1ce486c7ca1eb70f92d2d04edfb5
Luthfullah <luthfullah111@gmail.com>
2025-07-24 17:08:35 +0500

    twosum new changes added

--- twoSum.js
+++ twoSum.js
@@ -4,16 +4,24 @@
 // Given an array of integers nums and an integer target,
 //  return indices of the two numbers such that they add up to target.
 
-// Input: nums = [2, 7, 11, 15], target = 9
-// Output: [0, 1]
+// Input: nums = [2, 4, 3, 7, 10], target = 9
+// Output: [0, 3]
 
-function twoSum(nums, target) {
-  const map = {};
+const numbers = [2, 4, 3, 7, 10]; // Try edit me
+const target = 9;
+
+function twoSum(nums, tar) {
+  const map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    let complement = target - nums[i];
-    if (map.hasOwnProperty(complement)) {
-      return [map[complement], i];
+    const diff = tar - nums[i];
+    if (map.has(diff)) {
+      return [map.get(diff), i];
     }
-    map[nums[i]] = i;
+    map.set(nums[i], i);
   }
+  return map;
 }
+
+// Log to console
+console.log(twoSum(numbers, target));
+// Output: [0, 3]
